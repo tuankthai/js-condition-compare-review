45b020366cdf6dc8465f8ffaeb625a5697d9e318
dsl2022 <2015rpro@gmail.com>
2023-05-18 18:57:59 -0400

    first commit

---
+++ condition.test.js
@@ -0,0 +1,52 @@
+const {
+    checkNumber,
+    checkAge,
+    checkLeapYear,
+    findLargestNumber,
+  } = require("./solution.js");
+// Exercise 1
+describe("checkNumber", () => {
+    it("should display 'Positive number' when the number is positive", () => {
+      expect(checkNumber(5)).toBe("Positive number");
+    });
+  
+    it("should display 'Negative number' when the number is negative", () => {
+      expect(checkNumber(-5)).toBe("Negative number");
+    });
+  
+    it("should display 'Zero' when the number is zero", () => {
+      expect(checkNumber(0)).toBe("Zero");
+    });
+  });
+  
+  // Exercise 2
+  describe("checkAge", () => {
+    it("should display 'You are a minor' when the age is less than 18", () => {
+      expect(checkAge(15)).toBe("You are a minor");
+    });
+  
+    it("should display 'You are an adult' when the age is 18 or older", () => {
+      expect(checkAge(25)).toBe("You are an adult");
+    });
+  });
+  
+  // Exercise 3
+  describe("checkLeapYear", () => {
+    it("should display 'Leap year' for a leap year", () => {
+      expect(checkLeapYear(2020)).toBe("Leap year");
+    });
+  
+    it("should display 'Not a leap year' for a non-leap year", () => {
+      expect(checkLeapYear(2021)).toBe("Not a leap year");
+    });
+  });
+  
+  // Exercise 4
+  describe("findLargestNumber", () => {
+    it("should display the largest number among the three", () => {
+      expect(findLargestNumber(10, 5, 8)).toBe(10);
+      expect(findLargestNumber(5, 10, 8)).toBe(10);
+      expect(findLargestNumber(8, 5, 10)).toBe(10);
+    });
+  });
+  
---
+++ review-condition.js
@@ -0,0 +1,49 @@
+// Exercise 1
+function checkNumber(num) {
+  
+  // Write an if statement to check if the number is positive
+  // If it is positive, display "Positive number"
+  
+  // Write an else if statement to check if the number is negative
+  // If it is negative, display "Negative number"
+  
+  // Write an else statement for the case when the number is zero
+  // If it is zero, display "Zero"
+}
+
+// Exercise 2
+function checkAge(age) {
+  
+  // Write an if statement to check if the age is less than 18
+  // If it is less than 18, display "You are a minor"
+  
+  // Write an else statement for the case when the age is 18 or older
+  // If it is 18 or older, display "You are an adult"
+}
+
+// Exercise 3
+function checkLeapYear() {
+  // Prompt the user to enter a year
+  // Store the input in a variable
+  
+  // Write an if statement to check if the year is a leap year
+  // Use the conditions: divisible by 4 but not divisible by 100, unless divisible by 400
+  // If it is a leap year, display "Leap year"
+  
+  // Write an else statement for the case when the year is not a leap year
+  // If it is not a leap year, display "Not a leap year"
+}
+
+// Exercise 4
+function findLargestNumber(num1, num2, num3) {
+  
+  // Write an if statement to compare the numbers and find the largest one
+  // Display the largest number
+}
+
+module.exports = {
+    checkNumber,
+    checkAge,
+    checkLeapYear,
+    findLargestNumber,
+  };
